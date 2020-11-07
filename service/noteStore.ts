d4c45abb10c6019ded7b724b7515798705246887
UrsinZ <ursin.zimmermann@hsr.ch>
2020-11-07 06:48:10 +0100

    add db funcitons

--- service/noteStore.ts
+++ service/noteStore.ts
@@ -4,27 +4,27 @@ import {Note} from './Note'
 class NoteStore {
     private db:Datastore;
 
-    constructor(db?)
+    constructor(db?: Datastore)
     {
         this.db = db || new Datastore({filename: './data/notes.db', autoload: true});
     }
 
-    async create(note)
+    async create(note: Note)
     {
         return await this.db.insert(note);
     }
 
-    async update(id, note)
+    async update(id: number, note: Note)
     {
         return this.db.update({_id: id}, {$set: {...note}})
     }
 
-    async updateDone(id, done)
+    async updateDone(id: number, done: boolean)
     {
         return this.db.update({_id: id}, {$set: {done: done}})
     }
 
-    async delete(id)
+    async delete(id: number)
     {
         await this.db.update({_id: id}, {$set: {"state": "DELETED"}});
         return await this.get(id);
@@ -35,14 +35,38 @@ class NoteStore {
         return this.db.remove({}, {multi: true});
     }
 
-    async get(id)
+    async get(id: number)
     {
         return await this.db.findOne({_id: id});
     }
 
     async getAll()
     {
-        return await this.db.find({});
+        return this.db.find({});
+    }
+
+    async getSortedByImportance(ascending: boolean, includeFinished: boolean)
+    {
+        if(includeFinished)
+            return this.db.find({}).sort({_importance: ascending ? 1 : -1})
+
+        return this.db.find({_finished: {$ne: true}}).sort({_importance: ascending ? 1 : -1})
+    }
+
+    async getSortedByCreationDate(ascending: boolean, includeFinished:boolean)
+    {
+        if(includeFinished)
+            return this.db.find({}).sort({_creationDate: ascending ? 1 : -1})
+
+        return this.db.find({_finished: {$ne: true}}).sort({_creationDate: ascending ? 1 : -1})
+    }
+
+    async getSortedByDueDate(ascending: boolean, includeFinished:boolean)
+    {
+        if(includeFinished)
+            return this.db.find({}).sort({_dueDate: ascending ? 1 : -1})
+
+        return this.db.find({_finished: {$ne: true}}).sort({_dueDate: ascending ? 1 : -1})
     }
 }
 
